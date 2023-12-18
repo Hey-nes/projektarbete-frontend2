e9cb0145fa829fa29cbd21ac9a4407f654e0647e
Hey-nes <105350101+Hey-nes@users.noreply.github.com>
2023-12-18 11:28:39 +0100

    initial commit, added routes and nav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import NewTask from "./pages/NewTask";
+import Tasks from "./pages/Tasks";
+import NewHabit from "./pages/NewHabit";
+import Habits from "./pages/Habits";
+import Friends from "./pages/Friends";
+import { Link } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/tasks">Tasks</Link>
+          </li>
+          <li>
+            <Link to="/newhabit">New Habit</Link>
+          </li>
+          <li>
+            <Link to="/habits">Habits</Link>
+          </li>
+          <li>
+            <Link to="/friends">Friends</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/newtask" element={<NewTask />} />
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/newhabit" element={<NewHabit />} />
+        <Route path="/habits" element={<Habits />} />
+        <Route path="/friends" element={<Friends />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/pages/NewHabit.jsx
@@ -0,0 +1,5 @@
+const NewHabit = () => {
+  return <h1>New Habit</h1>;
+};
+
+export default NewHabit;
