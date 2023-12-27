855c6d26929b36e4ddf285a75a24ef0838483242
Hey-nes <105350101+Hey-nes@users.noreply.github.com>
2023-12-27 14:11:59 +0100

    integrated tasks with /home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,21 +3,35 @@ import { Link, json } from "react-router-dom";
 
 const Home = () => {
   const [recentFriends, setRecentFriends] = useState([]);
+  const [recentTasks, setRecentTasks] = useState([]);
 
   useEffect(() => {
-    const storedFriends = JSON.parse(localStorage.getItem("friends")) || [];
+    const storedFriends = localStorage.getItem("friends")
+      ? JSON.parse(localStorage.getItem("friends"))
+      : [];
     const lastFiveFriends = storedFriends.slice(-5);
     setRecentFriends(lastFiveFriends);
+
+    const storedTasks = localStorage.getItem("incompletedTasks")
+      ? JSON.parse(localStorage.getItem("incompletedTasks"))
+      : [];
+    const lastThreeTasks = storedTasks.slice(-3);
+    setRecentTasks(lastThreeTasks);
   }, []);
 
   return (
     <main className="main">
       <h1>Habitify - Organize Your Life</h1>
       <h2>Your Habits: </h2>
-      <Link to="/newHabit">
+      <Link to="/habits">
         <button>All Habits</button>
       </Link>
       <h3>Your Tasks: </h3>
+      <ul>
+        {recentTasks.map((task) => (
+          <li key={task.title}>{task.title}</li>
+        ))}
+      </ul>
       <Link to="/newTask">
         <button>All Tasks</button>
       </Link>
